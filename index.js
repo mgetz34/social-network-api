7c875e32c3593b696caf3810c359ef9cb93b8f4e
mgetz34 <mgetz340@gmail.com>
2023-01-03 09:45:26 -0700

    in addition to last commit, added the structure for the main index.js file, expres server, and index.js for the routes file

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const cwd = process.cwd();
 const PORT = 3001;
 const app = express();
 
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-    ? cwd.split('/01-Activities/')[1]
-    : cwd;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
