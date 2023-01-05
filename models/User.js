c47e107fa94b97dd23ca3454ac9816eebed1a477
mgetz34 <mgetz340@gmail.com>
2023-01-05 16:17:04 -0700

    added criteria to the user model and refs to other models

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: { type: string, required: true, uniquie: true, trim: true },
+        email: { type: string, required: true, uniquie: true, match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ },
+        throughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    // Getter
+    .get(function () {
+        return `${this.friendCount.length}`;
+    });
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
