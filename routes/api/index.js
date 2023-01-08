0da03ac06c28953d4ad2430826882a8f66007357
mgetz34 <mgetz340@gmail.com>
2023-01-08 14:57:39 -0700

    changed route prefix thought to thoughts in the index.js file for the routes dir, also removed deleteUser method from userRoutes.js for now untill all other routes are built out and functioning properly

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const UserRoutes = require('./userRoutes');
 
-router.use('./thought', thoughtRoutes);
-router.use('users', UserRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', UserRoutes);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,7 +3,6 @@ const {
     getUsers,
     getSingleUser,
     createUser,
-    deleteUser,
 } = require('../../controllers/userController');
 
 // /api/users
