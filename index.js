6a55ffe6268df22c4b9f0168fe44c9dc46a5d9e8
mgetz34 <mgetz340@gmail.com>
2023-01-08 14:30:11 -0700

    added shell code for the userRoutes and thoughtRoutes.js files

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
-
 const PORT = 3001;
 const app = express();
 
